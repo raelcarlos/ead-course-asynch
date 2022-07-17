e9ea0c24338c05dbdd0f319398183bd3392de220
Israel Carlos <israel.carlos@softplan.com.br>
2022-07-17 16:07:23 -0300

    comunicacao assincrona com authuser microservice via rabbitmq

---
+++ src/main/java/com/ead/course/consumers/UserConsumer.java
@@ -0,0 +1,42 @@
+package com.ead.course.consumers;
+
+import com.ead.course.dtos.UserEventDto;
+import com.ead.course.enums.ActionType;
+import com.ead.course.services.UserService;
+import com.netflix.appinfo.InstanceInfo;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import static com.ead.course.enums.ActionType.CREATE;
+
+@Component
+public class UserConsumer {
+
+    @Autowired
+    UserService userService;
+
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(value = "${ead.broker.queue.userEventQueue.name}", durable = "true"),
+            exchange = @Exchange(value = "${ead.broker.exchange.userEventExchange}", type = ExchangeTypes.FANOUT, ignoreDeclarationExceptions = "true"))
+    )
+    public void listenUserEvent(@Payload UserEventDto userEventDto){
+        var userModel = userEventDto.convertToUserModel();
+
+        switch (ActionType.valueOf(userEventDto.getActionType())){
+            case CREATE:
+            case UPDATE:
+                userService.save(userModel);
+                break;
+            case DELETE:
+                userService.delete(userEventDto.getUserId());
+                break;
+        }
+    }
+}
--- src/main/java/com/ead/course/specifications/SpecificationTemplate.java
+++ src/main/java/com/ead/course/specifications/SpecificationTemplate.java
@@ -23,14 +23,25 @@ public class SpecificationTemplate {
             @Spec(path = "courseStatus", spec = Equal.class),
             @Spec(path = "name", spec = Like.class)
     })
-    public interface CourseSpec extends Specification<CourseModel> {}
+    public interface CourseSpec extends Specification<CourseModel> {
+    }
+
+    @And({
+            @Spec(path = "email", spec = Like.class),
+            @Spec(path = "fullName", spec = Like.class),
+            @Spec(path = "userStatus", spec = Equal.class),
+            @Spec(path = "userType", spec = Equal.class)})
+    public interface UserSpec extends Specification<UserModel> {
+    }
 
 
     @Spec(path = "title", spec = Like.class)
-    public interface ModuleSpec extends Specification<ModuleModel> {}
+    public interface ModuleSpec extends Specification<ModuleModel> {
+    }
 
     @Spec(path = "title", spec = Like.class)
-    public interface LessonSpec extends Specification<LessonModel> {}
+    public interface LessonSpec extends Specification<LessonModel> {
+    }
 
     public static Specification<ModuleModel> moduleCourseId(final UUID courseId) {
         return (root, query, cb) -> {
@@ -52,4 +63,23 @@ public class SpecificationTemplate {
         };
     }
 
+    public static Specification<UserModel> userCourseId(final UUID courseId) {
+        return (root, query, cb) -> {
+            query.distinct(true);
+            Root<UserModel> user = root;
+            Root<CourseModel> course = query.from(CourseModel.class);
+            Expression<Collection<UserModel>> coursesUsers = course.get("users");
+            return cb.and(cb.equal(course.get("courseId"), courseId), cb.isMember(user, coursesUsers));
+        };
+    }
+
+    public static Specification<CourseModel> courseUserId(final UUID userId) {
+        return (root, query, cb) -> {
+            query.distinct(true);
+            Root<CourseModel> course = root;
+            Root<UserModel> user = query.from(UserModel.class);
+            Expression<Collection<CourseModel>> usersCourses = user.get("courses");
+            return cb.and(cb.equal(user.get("userId"), userId), cb.isMember(course, usersCourses));
+        };
+    }
 }
